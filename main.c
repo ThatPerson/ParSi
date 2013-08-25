ed5c97acd4a4c96d7f7a4cac6d7e81f3e5b8c287
Ben Tatman <ben@tatmans.co.uk>
2013-08-25 12:41:34 +0100

    Got the speed vectors working (look in output.txt at the 'Cannon', the speed eventually goes down, then it flips and starts heading down on a parabola) and I kept the is_collision functionality, however it now uses a brute force method (I had no way to get the line equations anymore as there may be parabolas :(

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ typedef struct {
 
 typedef struct {
 	char name[500];
-	float speed;
+	Force speed;
 	Force force;
 	Position pos;
 	int shown;
@@ -76,6 +76,14 @@ float to_degrees(float radians) {
 	//=B2/(D1/180)
 }
 
+float absol(float in) {
+	float new = in;
+	if (in < 0) {
+		//new = new - (2*in); // two times the number would get enough to make up for each side, then - because - - is +. For example, if in = -3, then we get -3 - (-3*2), so -3 - - 6, so -3 + 6, so 3.
+		new = 0 - in;
+	}
+	return new; //If value is +, then we do nothing.
+}
 
 Force new_force(float force, float angle) {
 	Force f;
@@ -109,14 +117,32 @@ Resolved resolve(Force f) {
 	return retur;
 }
 
-float absol(float in) {
-	float new = in;
-	if (in < 0) {
-		//new = new - (2*in); // two times the number would get enough to make up for each side, then - because - - is +. For example, if in = -3, then we get -3 - (-3*2), so -3 - - 6, so -3 + 6, so 3.
-		new = 0 - in;
+Force anti_resolve(Resolved f) {
+	Force p;
+	
+	p.force = sqrt(power(f.x.force, 2) + power(f.y.force, 2));
+
+	if ((f.x.force < 0)	&& (f.y.force < 0)) {
+		// it matches >= 180 < 270
+		p.angle = atan((absol(f.x.force)/absol(f.y.force)));
+		p.angle = 180 + to_degrees(p.angle);
+	} else if ((f.x.force < 0) && (f.y.force >= 0)) {
+		// then it is in the final one, or > 270
+		p.angle = atan(f.y.force/absol(f.x.force));
+		p.angle = 270 + to_degrees(p.angle);
+	} else if ((f.x.force >= 0) && (f.y.force < 0)) {
+		// then it is >= 90, < 180
+		p.angle = atan(absol(f.y.force)/f.x.force);
+		p.angle = 90 + to_degrees(p.angle);
+	} else {
+		// then it is the first one, or 0 < x < 90
+		p.angle = atan(f.x.force/f.y.force);
+		p.angle = to_degrees(p.angle);
 	}
-	return new; //If value is +, then we do nothing.
+	return p;
 }
+
+
 
 Force balance_force(Force a, Force b) {
 
@@ -137,17 +163,13 @@ Force balance_force(Force a, Force b) {
 	float tmpangle;
 	//printf("New %f %f\n", absol(new.x), absol(new.y));
 	if (new.x >= 0 && new.y >= 0) {
-		printf("0 - 90\n");
 		tmpangle = to_degrees(atan(absol(new.x)/absol(new.y)));
 		// We are in the top right
 	} else if (new.x >= 0 && new.y <= 0) {
-		printf("90 - 180\n");
 		tmpangle = to_degrees(atan(absol(new.y)/absol(new.x))) + 90;
 	} else if (new.x <= 0 && new.y <= 0) {
-		printf("180 - 270\n");
 		tmpangle = to_degrees(atan(absol(new.x)/absol(new.y))) + 180;
 	} else if (new.x <= 0 && new.y >= 0) {
-		printf("270 - 360\n");
 		tmpangle = to_degrees(atan(absol(new.y)/absol(new.x))) + 270;
 	}
 	result.angle = tmpangle;
@@ -161,10 +183,13 @@ Position wait(Particle a, float time) {
 	//s = 1/2at^2
 
 	//We need to resolve the speed of the particle into x and y to use in s=ut+1/2at^2. Because resolve can do this, we just make a new Force with the speed as the force
-	Resolved p = resolve(new_force(a.speed, a.force.angle));
+	Resolved p = resolve(a.speed);
 
-	xinc = (p.x.force*time) + ((x.x.force*time*time)/2);
-	yinc = (p.y.force*time) + ((x.y.force*time*time)/2);
+	//xinc = (p.x.force*time) + ((x.x.force*time*time)/2);
+	//yinc = (p.y.force*time) + ((x.y.force*time*time)/2);
+	// What on earth went on there?
+	xinc = (p.x.force*time) + ((x.x.force/2)*(time*time));
+	yinc = (p.y.force*time) + ((x.y.force/2)*(time*time));
 	Position l;
 	l.x = a.pos.x + xinc;
 	l.y = a.pos.y + yinc;
@@ -193,59 +218,34 @@ Position posdiff(Position a, Position b) {
 	return l;
 }
 
-TestCase is_collision(TestCase q) {
-	/*
-	 * This uses a graphing mechanism. Basically it works out the equation of the line of a particle, and 
-	 * uses this to work out the intercept. Then, using Pythag it works out the distance for each to reac
-	 * h said point, and then uses t = (-u + sqrt(2sa+u^2))/a to work out the time required. Then, it com
-	 * pares the two times, and if they are the same (well, 1/1000th margin for change) it will regard it 
-	 * as true
-	 */
-	// Firstly we do A
-	Resolved a = resolve(q.a.force);
-	float gradianta = a.y.force/a.x.force;
-	float ac = q.a.pos.y - (gradianta*q.a.pos.x); // y-px = c, use the position values as y and x
-	// hence, formula = y = gradiant * x + ac
-	// And now we do B
-	Resolved b = resolve(q.b.force);
-	float gradiantb = b.y.force/b.x.force;
-	float bc = q.b.pos.y - (gradiantb*q.b.pos.x);
-
-	// Now,   gradianta*x + ac = gradiantb*x + bc
-	// Hence, gradianta*x - gradiantb*x = bc - ac
-	//        x*(gradianta-gradiantb) = bc - ac
-	//        x = (bc - ac)/(gradianta-gradiantb)
-	float x = (bc - ac)/(gradianta-gradiantb);
-	float y = (gradianta * x) + ac; // Well, if y=gradianta*x+ac, why not just use it here?
-
-	// Now we find out the distances for each
-	float ax = absol(q.a.pos.x - x);
-	float ay = absol(q.a.pos.y - y);
-	float ac2 = (power(ax,2)) + (power(ay,2));
-	float al = sqrt(ac2);
-
-	// And for B
-	float bx = absol(q.b.pos.x - x);
-	float by = absol(q.b.pos.y - y);
-	float bc2 = power(bx,2) + power(by,2);
-	float bl = sqrt(bc2);
-
-	// So, we have s values for each. u was given to us (q.a.speed), as was a (q.a.force.force) so we can go ahead
-	float at = ((-q.a.speed + sqrt((2*al*q.a.force.force)+(power(q.a.speed,2))))/q.a.force.force);
-	float bt = ((-q.b.speed + sqrt((2*bl*q.b.force.force)+(power(q.b.speed,2))))/q.b.force.force);
-	at = ceilf(at * 100) / 100;
-	bt = ceilf(bt * 100) / 100;
-	if (at == bt) {
-		q.time = at;
-		q.is_collision = 1;
-	} else {
-		q.time = 0;
-		q.is_collision = 0;
-	}
+TestCase is_collision(TestCase q, float within, float precision) {
+	float i;
+	TestCase p = q;
+	int curr_num, curr_den;
+	float xdiff, ydiff;
+	for (i = 0; i < within;) {
+		p.a.pos = wait(q.a, i);
+		p.b.pos = wait(q.b, i);
+		//printf("%f\n", i/precision);
+		q.a.pos.x = ceilf(q.a.pos.x * precision) / precision;
+		q.a.pos.y = ceilf(q.a.pos.y * precision) / precision;
+		q.b.pos.x = ceilf(q.b.pos.x * precision) / precision;
+		q.b.pos.y = ceilf(q.b.pos.y * precision) / precision;
+		xdiff = p.a.pos.x - p.b.pos.x;
+		xdiff = absol(xdiff);
+		ydiff = p.a.pos.y - p.b.pos.y;
+		ydiff = absol(ydiff);
+		if ((ydiff < 0.1) && (xdiff < 0.1)) {
+		
+			q.is_collision = 1;
+			q.time = i;
+			return q;
+		}
+		i = i + within/precision;
+	}	
 	return q;
 }
-
-
+	
 
 Particle new_particle(float x, float y, float force, float angle) {
 	Particle t;
@@ -253,6 +253,7 @@ Particle new_particle(float x, float y, float force, float angle) {
 	t.pos.y = y;
 	t.force.force = force;
 	t.force.angle = angle;
+	t.speed.angle = angle;
 	t.shown = 1;
 	return t;
 }
@@ -266,37 +267,51 @@ Position new_position(float x, float y) {
 }
 
 void tabulate_particles(Particle p[], int count, float time) {
-	printf("%10s %10s %10s %10s %10s %10s %10s\n", "Time", "Name", "X", "Y", "Force", "Angle", "Speed");
+	printf("%10s %10s %10s %10s %10s %10s %10s %10s\n", "Time", "Name", "X", "Y", "Force", "Angle", "Speed", "SAngle");
 	int i;
-	for (i = 0; i < 76; i++) {
+	for (i = 0; i < 87; i++) {
 		printf("-");
 	}
 	printf("\n");
 	for (i = 0; i < count; i++) {
 		if (p[i].shown == 1) {
-			printf("%10f %10s %10.2f %10.2f %10.2f %10.2f %10.2f\n", time, p[i].name, p[i].pos.x, p[i].pos.y, p[i].force.force, p[i].force.angle, p[i].speed);
+			printf("%10f %10s %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f\n", time, p[i].name, p[i].pos.x, p[i].pos.y, p[i].force.force, p[i].force.angle, p[i].speed.force, p[i].speed.angle);
 		}
 	}
 	printf("\n");
 }	
 
-float get_speed(Particle a, float time) {
+Force get_speed(Particle a, float time) {
 	// v = u + at. a = a.force.force. t = time. u = a.speed.
-	return a.speed + (a.force.force*time);
+	
+	Resolved q = resolve(a.speed);
+	Resolved p = resolve(a.force);
+	
+	float xsp = q.x.force + (p.x.force*time);
+	float ysp = q.y.force + (p.y.force*time);
+	
+	
+	Resolved l;
+	l.x.force = xsp;
+	l.y.force = ysp;
+	l.x.angle = 90;
+	l.y.angle = 0;
+	return anti_resolve(l);
 }
 
 void wait_all(Particle p[], int count, float time, float display_time) {
 	int i,o;
 	float waittime = time;
 	TestCase watermelon;
+	watermelon.is_collision = 0;
 	for (i = 0; i < count; i ++) {
 		if (p[i].shown == 1) {
 			waittime = time;
 			watermelon.a = p[i];
-			for (o = 0; o < count; o++) {
+			for (o = i+1; o < count; o++) {
 				if (p[o].shown == 1) {
 					watermelon.b = p[o];
-					watermelon = is_collision(watermelon);
+					watermelon = is_collision(watermelon, time, 10);
 					if (watermelon.is_collision == 1) {
 
 						if (watermelon.time < (2*time)) {
@@ -304,13 +319,14 @@ void wait_all(Particle p[], int count, float time, float display_time) {
 							p[i].pos = wait(p[i], watermelon.time);
 							p[o].shown = 0;
 							p[i].force = balance_force(p[i].force, p[o].force);
+							p[i].speed = balance_force(p[i].speed, p[o].speed);
 						}
 					}
 				}
 			}
 			p[i].pos = wait(p[i], waittime);
 			p[i].speed = get_speed(p[i], waittime);
-
+			watermelon.is_collision = 0;
 		}
 	}
 	tabulate_particles(p, count, display_time);
@@ -318,8 +334,11 @@ void wait_all(Particle p[], int count, float time, float display_time) {
 
 int main(int argc, char * argv[]) {
 	
+	Resolved q = resolve(new_force(4, 30));
+	print_force(anti_resolve(q));
+	
 	Particle lo = new_particle(0,0,2,90);
-	lo.speed = 2;
+	lo.speed.force = 2;
 	lo.pos = wait(lo, 0.05);
 	printf("%f\n", lo.pos.x);
 
@@ -328,24 +347,29 @@ int main(int argc, char * argv[]) {
 
 	p[0] = new_particle(0,0,4,45);
 	strcpy(p[0].name,"Test");
-	p[0].speed = 0;
+	p[0].speed.force = 0;
 
 	Position lq = wait(p[0], 3);
 	print_position(lq);
 
 	p[1] = new_particle(25,0,4,315);
 	strcpy(p[1].name,"Sim");
-	p[1].speed = 0;
+	p[1].speed.force = 0;
 
-	p[2] = new_particle(0, 30, 20, 90);
-	p[2].speed = 0;
+	p[2] = new_particle(0,25, 9.8, 180);
+	strcpy(p[2].name, "Cannon");
+	p[2].speed.force = 4;
+	p[2].speed.angle = 45;
+	
+/*	p[2] = new_particle(0, 30, 20, 90);
+	p[2].speed.force = 0;
 	strcpy(p[2].name, "Followup");
 	p[3] = new_particle(0, 40, 2, 90);
 	strcpy(p[3].name, "Miss");
-	p[3].speed = 0;
+	p[3].speed.force = 0;*/
 	int i;
-	for (i = 0; i < 90; i++) {
-		wait_all(p, 4, 0.1, i*0.1);
+	for (i = 0; i < 30; i++) {
+		wait_all(p, 3, 0.1, i*0.1);
 	}
 }
 
