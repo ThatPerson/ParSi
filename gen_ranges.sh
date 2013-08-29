2fe27123df7f4059ae3073084008c104537eabb8
Ben Tatman <ben@tatmans.co.uk>
2013-08-29 09:34:23 +0100

    Delete gen_ranges.sh

--- gen_ranges.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm output.txt output.csv output_radians.csv output_radians.txt
-./main 0 0 >> output.txt
-./main 1 0 >> output.csv
-./main 0 1 >> output_radians.txt
-./main 1 1 >> output_radians.csv
